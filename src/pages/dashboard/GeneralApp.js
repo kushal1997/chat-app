35139196daa97e79cf25e1edbc0ebac85ad6de3f
kushal1997 <kushalrao103@gmail.com>
2024-03-14 23:57:01 +0530

    conversation part done

--- src/pages/dashboard/GeneralApp.js
+++ src/pages/dashboard/GeneralApp.js
@@ -1,11 +1,23 @@
 import React from "react";
 import { Chats } from "./Chats";
+import { Box, Stack } from "@mui/material";
+import { Conversation } from "../../components/Conversation";
+import {useTheme } from "@mui/material/styles";
 
 const GeneralApp = () => {
-
+  const theme = useTheme();
   return (
     <>
-      <Chats/>
+      <Stack direction="row" width={"100%"}>
+        <Chats />
+        <Box
+          sx={{
+            height: "100%", width: "80%", backgroundColor: theme.palette.mode === 'light' ? "#fff" : theme.palette.background.default
+          }}
+        >
+          <Conversation/>
+        </Box>
+      </Stack>
     </>
   );
 };
